89f4315bdca6b6526affee6946f1753ebf305186
Efmv1 <efmv1@ya.ru>
2024-05-30 02:03:52 +0300

    Задание выполнено

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countButton: UIButton!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    private var counter: Int = 0
+    
+    @IBAction func buttonDidTouched(_ sender: UIButton) {
+        counter += 1
+        counterLabel.text = """
+Значение счетчика:
+\(counter)
+"""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter = 0
+        counterLabel.text = """
+Значение счетчика:
+\(counter)
+"""
     }
 
 
